e56b5d0f8206e1e0f3713bd81355a692958bec63
LazzouziYoussefEtu <youssef.lazzouzi@etu.uae.ac.ma>
2025-11-26 09:30:25 +0100

    fix(app): make AppComponent standalone and import HeaderComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
-import { RouterOutlet, RouterLink } from '@angular/router'; // <--- Import du routeur
+import { RouterOutlet } from '@angular/router'; // <--- Import du routeur
+import { HeaderComponent } from './header/header.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink], // <--- Ajoutez RouterLink ici
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
