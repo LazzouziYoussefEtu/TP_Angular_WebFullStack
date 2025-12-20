ba63216f11681e3327e89b47593be548a7a67960
LazzouziYoussefEtu <youssef.lazzouzi@etu.uae.ac.ma>
2025-12-20 17:46:54 +0100

    Refactor: Use new PNG logos and ensure theme reactivity across components
    - Switch to angular_gradient.png for light mode and angular_white.png for dark mode
    - Add theme detection and MutationObserver to HomeComponent for logo reactivity
    - Finalize monocolored header icons fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -10,6 +10,23 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isDarkTheme: boolean = false;
 
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkTheme = this.document.body.classList.contains('dark-theme');
+      
+      // Observe changes to the body class
+      const observer = new MutationObserver(() => {
+        this.isDarkTheme = this.document.body.classList.contains('dark-theme');
+      });
+      observer.observe(this.document.body, { attributes: true, attributeFilter: ['class'] });
+    }
+  }
 }
